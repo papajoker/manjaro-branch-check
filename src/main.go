07bd6c4d6a52d49907d1aa7e1eeeeef1ebd893a9
papajoke <papajoke@manjaro.fr>
2025-03-31 20:08:16 +0200

    defaut : use extra repo

--- src/main.go
+++ src/main.go
@@ -1,36 +1,13 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+Copyright © 2025 papajoker
 */
 package main
 
 import (
-	"fmt"
 	"mbc/cmd"
-	"runtime/debug"
 )
 
-func getAppName() string {
-	info, ok := debug.ReadBuildInfo()
-	if ok {
-		for _, dep := range info.Deps {
-			fmt.Println(dep)
-			if dep.Sum == "" {
-				return dep.Path
-			}
-		}
-	}
-	return "" //os.Args[0]
-}
-
 func main() {
-	/*
-		app := os.Args[0]
-		fmt.Println("app:", app, "# pas avec go run")
-		fmt.Println("app:", getAppName(), "# PAS avec le build")
 
-		fmt.Println("inclu a la compilation maison: ", Project)
-		fmt.Printf("\n%s Version: %v %v %v %v\n", filepath.Base(os.Args[0]), Version, GitID, GitBranch, BuildDate)
-		fmt.Println(strings.Repeat("#", 30))
-	*/
 	cmd.Execute()
 }
