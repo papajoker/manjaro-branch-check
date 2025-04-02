1d780bd9f2c09e7c31f9079d5a9d336e79fc8226
papajoke <papajoke@manjaro.fr>
2025-04-02 13:08:54 +0200

    alpm: refactoring

--- src/cmd/alpm/db.go
+++ src/cmd/alpm/db.go
@@ -25,13 +25,7 @@ type (
 		DESC      string
 		REPO      string
 		BUILDDATE time.Time
-		//URL      string
-		PACKAGER string
-		//REPLACES []string
-		//PROVIDES []string
-
-		//IsDep bool
-		//ReplacedBy string
+		PACKAGER  string
 	}
 
 	Packages map[string]*Package
@@ -40,27 +34,30 @@ type (
 // parse desc file content
 func (p *Package) set(desc string) bool {
 	tmpdesc := strings.Split(desc, "\n\n")
-	adesc := make(tdesc)
+	//adesc := make(tdesc)
+	adesc := make(tdesc, len(tmpdesc))
 	for i := range tmpdesc {
 		tmp := strings.Split(tmpdesc[i], "\n")
-		idx := strings.Replace(tmp[0], "%", "", -1)
+		if len(tmp) == 0 || len(tmp[0]) < 5 {
+			continue
+		}
+
+		idx := strings.TrimRight(tmp[0][1:], "%")
 		if len(tmp) > 1 {
 			adesc[idx] = tmp[1:]
 		} else {
-			adesc[idx] = make([]string, 0)
+			adesc[idx] = []string{}
 		}
 	}
 
 	p.VERSION = getFieldString(adesc, "VERSION")
 	p.NAME = getFieldString(adesc, "NAME")
-	p.DESC = getFieldString(adesc, "DESC")
-	timestamp := int64(getFieldInt(adesc, "BUILDDATE"))
-	p.BUILDDATE = time.Unix(timestamp, 0)
+	p.BUILDDATE = getFieldDate(adesc, "BUILDDATE")
+	p.PACKAGER = getFieldString(adesc, "PACKAGER")
+	//p.DESC = getFieldString(adesc, "DESC")
 	//p.URL = getFieldString(adesc, "URL")
 	//p.REPLACES = getFieldArray(adesc, "REPLACES")
 	//p.PROVIDES = getFieldArray(adesc, "PROVIDES")
-	p.PACKAGER = getFieldString(adesc, "PACKAGER")
-	//p.IsDep = getFieldString(adesc, "REASON") == "1"
 	return true
 }
 
@@ -71,16 +68,18 @@ func (p Package) String() string {
  Date:     %s`, p.NAME, p.VERSION, d)
 }
 
+/*
 func (p Package) Desc(maxi int) string {
 	if maxi > len(p.DESC) {
 		return p.DESC
 	}
 	return p.DESC[:maxi-1] + "…"
-
 }
+*/
 
 func getFieldString(adesc tdesc, key string) string {
-	if len(adesc[key]) < 1 {
+	values, ok := adesc[key]
+	if !ok || len(values) < 1 {
 		return ""
 	}
 	return strings.TrimSpace(adesc[key][0])
@@ -99,14 +98,19 @@ func getFieldArray(adesc tdesc, key string) []string {
 */
 
 func getFieldInt(adesc tdesc, key string) int {
-	if items, ok := adesc[key]; ok {
-		item := items[0]
-		i, err := strconv.Atoi(item)
-		if err == nil {
+	if items, ok := adesc[key]; ok && len(items) > 0 {
+		if i, err := strconv.Atoi(items[0]); err == nil {
 			return i
 		}
 	}
 	return -1
+}
+
+func getFieldDate(adesc tdesc, key string) time.Time {
+	if timestamp := int64(getFieldInt(adesc, key)); timestamp > -1 {
+		return time.Unix(timestamp, 0)
+	}
+	return time.Time{}
 }
 
 func ExtractTarGz(gzipStream io.Reader, pkgs Packages, repo string) Packages {
@@ -118,6 +122,7 @@ func ExtractTarGz(gzipStream io.Reader, pkgs Packages, repo string) Packages {
 		fmt.Println(errMsg, err.Error())
 		return pkgs
 	}
+	defer uncompressedStream.Close()
 
 	tarReader := tar.NewReader(uncompressedStream)
 
@@ -132,51 +137,44 @@ func ExtractTarGz(gzipStream io.Reader, pkgs Packages, repo string) Packages {
 			log.Fatalf("ExtractTarGz: Next() failed: %s", err.Error())
 		}
 
-		switch header.Typeflag {
-		case tar.TypeDir:
-			/*fmt.Println("::dir:",header.Name)*/
-		case tar.TypeReg:
+		if header.Typeflag != tar.TypeReg {
+			continue
+		}
 
-			buf := new(bytes.Buffer)
+		buf := new(bytes.Buffer)
 
-			if _, err := buf.ReadFrom(tarReader); err != nil {
-				if err != io.EOF {
-					//nb = nb + 1
-					fmt.Println(errMsg, err.Error())
-					log.Fatalf("ExtractTarGz:  failed: %s", err.Error())
-				}
+		if _, err := io.Copy(buf, tarReader); err != nil && err != io.EOF {
+			fmt.Fprintln(os.Stderr, errMsg, err.Error())
+			log.Fatalf("ExtractTarGz:  failed: %s", err.Error())
+		}
+
+		pkg := Package{REPO: repo}
+		if pkg.set(buf.String()) {
+			if _, ok := pkgs["pkg.NAME"]; ok {
+				fmt.Fprintf(os.Stderr, "\t # %s : %s\n", gotext.Get("ignore duplicate"), pkg.NAME)
+			} else {
+				pkgs[pkg.NAME] = &pkg
 			}
-
-			pkg := Package{REPO: repo}
-			if pkg.set(buf.String()) {
-				if _, ok := pkgs["pkg.NAME"]; ok {
-					fmt.Printf("\t # %s : %s\n", gotext.Get("ignore duplicate"), pkg.NAME)
-				} else {
-					pkgs[pkg.NAME] = &pkg
-				}
-			}
-		default:
-			fmt.Println(errMsg, "def", header.Typeflag, header.Name)
-			fmt.Printf("ExtractTarGz: uknown type: %v in %s\n", header.Typeflag, header.Name)
-			os.Exit(8)
 		}
 	}
 	return pkgs
 }
 
+// load one branch
 func Load(dirPath string, repos []string) (pkgs Packages) {
 	pkgs = make(Packages, 5000)
 	for _, repo := range repos {
 		nb := len(pkgs)
-		//fmt.Printf("%v# %s ...%v\t", theme.ColorGray, repo, theme.ColorNone)
 		f, err := os.Open(filepath.Join(dirPath, repo+".db"))
 		if err != nil {
 			fmt.Printf("Error: can't read file %s\n", filepath.Join(dirPath, repo+".db"))
 			//os.Exit(1)
 			continue
 		}
-		defer f.Close()
+
 		pkgs = ExtractTarGz(f, pkgs, repo)
+		f.Close()
+
 		if len(pkgs)-nb == 0 {
 			sync := "sync"
 			if strings.Contains(dirPath, "/var/lib/") {
@@ -189,7 +187,6 @@ func Load(dirPath string, repos []string) (pkgs Packages) {
 				gotext.Get("repo empty ? or all packages are ignored"),
 			)
 		}
-		//fmt.Println(repo, len(pkgs)-nb, "packages")
 	}
 	return pkgs
 }
